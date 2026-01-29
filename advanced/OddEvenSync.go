bb8e2cf77461d475dba8d4f7fae52ba439147ca4
Vikas Singh <56144808+vikaassingh@users.noreply.github.com>
2026-01-29 15:07:16 +0530

    Refactor OddEvenSync for improved concurrency

--- advanced/OddEvenSync.go
+++ advanced/OddEvenSync.go
@@ -1,42 +1,64 @@
 package advanced
 
+package main
+
 import (
 	"fmt"
 	"sync"
 )
 
-func OddEvenSync() {
+var (
+	oddTurn  = make(chan struct{}, 1)
+	evenTurn = make(chan struct{}, 1)
+)
+
+func main() {
 	ch := make(chan int)
 	wg := &sync.WaitGroup{}
-	wg.Add(2)
 
-	go workerOdd(ch, wg)
-	ch <- 1
-	go workerEven(ch, wg)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 10; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	wg.Add(2)
+	oddTurn <- struct{}{}
+	go evenWorker(ch, wg)
+	go oddWorker(ch, wg)
 
 	wg.Wait()
+	close(oddTurn)
+	close(evenTurn)
 }
 
-func workerOdd(ch chan int, wg *sync.WaitGroup) {
+func oddWorker(ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for num := range ch {
-		fmt.Println(num, " : From Odd")
-		if num < 10 {
-			ch <- num + 1
+	for {
+		<-oddTurn
+		num, ok := <-ch
+		if !ok {
+			evenTurn <- struct{}{}
+			return
 		}
+		fmt.Println("O:", num)
+		evenTurn <- struct{}{}
 	}
 }
 
-func workerEven(ch chan int, wg *sync.WaitGroup) {
+func evenWorker(ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for num := range ch {
-		fmt.Println(num, " : From Even")
-		if num < 10 {
-			ch <- num + 1
-		}
-		if num == 10 {
-			close(ch)
+	for {
+		<-evenTurn
+		num, ok := <-ch
+		if !ok {
+			oddTurn <- struct{}{}
 			return
 		}
+		fmt.Println("E:", num)
+		oddTurn <- struct{}{}
 	}
 }
