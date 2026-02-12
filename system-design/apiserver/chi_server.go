83024e26aae530ab4157ae78c602829cca7e4312
Vikas Singh <56144808+vikaassingh@users.noreply.github.com>
2026-02-12 10:57:12 +0530

    Refactor chi_server for improved readiness handling

--- system-design/apiserver/chi_server.go
+++ system-design/apiserver/chi_server.go
@@ -1,87 +1,128 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os/signal"
-	"sync"
-	"syscall"
-	"time"
-
-	ch "github.com/go-chi/chi/v5"
-)
+import "fmt"
 
 type Server struct {
-	host    string
-	port    string
-	handler http.Handler
-	server  *http.Server
+	host string
+	port string
+	server *http.Server
+	isReady atomic.Bool
+	isDraining atomic.Bool
+	activeRequest atomic.Int64
 }
 
 func main() {
-	svr := &Server{}
+	svr := &Server{
+		host : "localhost",
+		port : "8080",
+	}
 	svr.Run()
 }
 
 func (s *Server) Run() {
-	s.host = "localhost"
-	s.port = "8080"
+	s.SetupServer()
 	s.Start()
 }
 
-func (s *Server) Start() {
-	s.server = s.NewServer()
-	ctx, stop := signal.NotifyContext(
-		context.Background(),
-		syscall.SIGINT,
-		syscall.SIGTERM,
-	)
-	defer stop()
+func (s *Server) SetupServer() {
+	routes := chi.NewRouter()
+	routes.Use(s.requestMiddleware)
+	s.RegisterRoutes(routes)
+	s.server = &http.Server{
+		Addr : s.host + ":" + s.port,
+		Handler : routes,
+	}
+	s.isReady.Store(true)
+}
 
-	wg := &sync.WaitGroup{}
-	// Start server
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		log.Printf("Server starting on %s:%s", s.host, s.port)
-		if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen error: %v", err)
+func (s *Server) requestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if s.isDraining.Load() {
+			http.Error(w, "Server draining", http.StatusServiceUnavailable)
+			return
 		}
-	}()
-
-	// Waiting for shutdown signal
-	<-ctx.Done()
-	log.Println("Shutdown signal received")
-	s.Stop()
-
-	wg.Wait()
-	fmt.Println("Server exiting")
+		
+		s.activeRequest.Add(1)
+		defer s.activeRequest.Add(-1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (s *Server) NewServer() *http.Server {
-	router := ch.NewRouter()
-	s.Routes(router)
-	return &http.Server{
-		Addr:    s.host + ":" + s.port,
-		Handler: router,
-	}
-}
-
-func (s *Server) Stop() {
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	// Gracefully shutdown server
-	if err := s.server.Shutdown(shutdownCtx); err != nil {
-		log.Fatalf("Server shutdown failed:%+v", err)
-	} else {
-		log.Println("Server shutdown gracefully")
-	}
-}
-
-func (s *Server) Routes(r *ch.Mux) {
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+func (s *Server) RegisterRoutes(r chi.Router) {
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte("OK"))
 	})
+	
+	r.Get("/ready", s.readynessHandler)
+}
+
+func (s *Server) readynessHandler(w http.ResponseWriter, r *http.Request) {
+	if !s.isReady.Load() {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		w.Write([]byte("not ready"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ready"))
+}
+
+func (s *Server) Start() {
+	intSignal, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func (){
+		defer wg.Done()
+		log.Printf("Server started listening on %v:%v", s.host, s.port)
+		if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("ListenAndServe error:%v", err)
+		} else {
+			log.Printf("Server listening on %s:%s", s.host, s.port)
+		}
+	}()
+	
+	// Waiting for stop signal
+	<-intSignal.Done()
+	log.Println("Shutdown signal received")
+	
+	s.GracefullyShutdown()
+	
+	wg.Wait()
+	log.Println("Exiting server")
+}
+
+func (s *Server) GracefullyShutdown() {
+	log.Println("Marking not ready")
+	s.isReady.Store(false)
+	
+	log.Println("Entering draining mod")
+	s.isDraining.Store(true)
+	
+	time.Sleep(5 * time.Second)
+	
+	s.server.SetKeepAlivesEnabled(false)
+	
+	waitStart := time.Now()
+	for {
+		active := s.activeRequest.Load()
+		if active == 0 {
+			break
+		}
+		
+		log.Printf("Waiting for %d active requests...", active)
+		time.Sleep(200 * time.Millisecond)
+	}
+	log.Printf("All active request completed in %v", time.Since(waitStart))
+	
+	s.Shutdown()
+}
+
+func (s *Server) Shutdown() {	
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err := s.server.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Gracefully shutdown failed: %v", err)
+	} else {
+		log.Printf("Gracefully shutdown successfully")
+	}
 }
