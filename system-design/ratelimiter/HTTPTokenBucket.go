cc92ce16f79684f7a218452e032e07863b0b98a4
Vikas Singh <56144808+vikaassingh@users.noreply.github.com>
2026-04-27 19:02:12 +0530

    Update HTTPTokenBucket.go

--- system-design/ratelimiter/HTTPTokenBucket.go
+++ system-design/ratelimiter/HTTPTokenBucket.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"math"
-	"net"
+	"math/rand"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 )
@@ -22,18 +22,64 @@ type RateLimiter struct {
 	maxTokens  float64
 	refillRate float64
 	ttl        time.Duration
+	ctx        context.Context
 }
 
-func NewRateLimiter(maxTokens int, refillRate float64, ttl time.Duration) *RateLimiter {
+func NewRateLimiyter(maxTokens, refillRate float64, ttl time.Duration, ctx context.Context) *RateLimiter {
 	rl := &RateLimiter{
 		buckets:    make(map[string]*bucket),
-		maxTokens:  float64(maxTokens),
+		maxTokens:  maxTokens,
 		refillRate: refillRate,
 		ttl:        ttl,
+		ctx:        ctx,
 	}
 
 	go rl.cleanup()
 	return rl
+}
+
+func (rl *RateLimiter) cleanup() {
+	ticker := time.NewTicker(rl.ttl)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			rl.mu.Lock()
+			now := time.Now()
+
+			for k, b := range rl.buckets {
+				if now.Sub(b.lastRefill) > rl.ttl {
+					log.Println("Deleting key:", k)
+					delete(rl.buckets, k)
+				}
+			}
+			rl.mu.Unlock()
+		case <-rl.ctx.Done():
+			fmt.Println("cleanup stoped")
+			return
+		}
+	}
+}
+
+func (rl *RateLimiter) Apply(key string) (bool, float64) {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+	b := rl.getBucket(key)
+	now := time.Now()
+	elapsed := now.Sub(b.lastRefill).Seconds()
+	tokens := b.tokens + (elapsed * rl.refillRate)
+	if rl.maxTokens < tokens {
+		tokens = rl.maxTokens
+	}
+	b.tokens = tokens
+	b.lastRefill = now
+	if b.tokens > 0 {
+		b.tokens--
+		return true, b.tokens
+	}
+
+	return false, b.tokens
 }
 
 func (rl *RateLimiter) getBucket(key string) *bucket {
@@ -43,66 +89,17 @@ func (rl *RateLimiter) getBucket(key string) *bucket {
 			tokens:     rl.maxTokens,
 			lastRefill: time.Now(),
 		}
+
 		rl.buckets[key] = b
 	}
+
 	return b
 }
 
-func (rl *RateLimiter) Allow(key string) (bool, float64) {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
-
-	now := time.Now()
-	b := rl.getBucket(key)
-
-	elapsed := now.Sub(b.lastRefill).Seconds()
-	b.tokens = math.Min(rl.maxTokens, b.tokens+elapsed*rl.refillRate)
-	b.lastRefill = now
-
-	if b.tokens >= 1 {
-		b.tokens -= 1
-		return true, b.tokens
-	}
-
-	return false, b.tokens
-}
-
-// Cleanup old buckets to prevent memory leak
-func (rl *RateLimiter) cleanup() {
-	ticker := time.NewTicker(rl.ttl)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		rl.mu.Lock()
-		now := time.Now()
-
-		for k, b := range rl.buckets {
-			if now.Sub(b.lastRefill) > rl.ttl {
-				delete(rl.buckets, k)
-			}
-		}
-		rl.mu.Unlock()
-	}
-}
-
-// Extract client IP safely
-func getIP(r *http.Request) string {
-	ip := r.Header.Get("X-Forwarded-For")
-	if ip != "" {
-		parts := strings.Split(ip, ",")
-		return strings.TrimSpace(parts[0])
-	}
-
-	ip, _, _ = net.SplitHostPort(r.RemoteAddr)
-	return ip
-}
-
-// Middleware
 func (rl *RateLimiter) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		key := getIP(r)
-
-		allowed, tokens := rl.Allow(key)
+		randUser := GetRandUser(1000)
+		allowed, tokens := rl.Apply(randUser)
 		if !allowed {
 			retryAfter := int(math.Ceil(1 / rl.refillRate))
 
@@ -111,24 +108,28 @@ func (rl *RateLimiter) Middleware(next http.Handler) http.Handler {
 			return
 		}
 
-		// Optional headers
 		w.Header().Set("X-RateLimit-Remaining", fmt.Sprintf("%.0f", tokens))
-
 		next.ServeHTTP(w, r)
 	})
 }
 
+func GetRandUser(randUser int) string {
+	return fmt.Sprintf("x-User-ID-%v", rand.Intn(randUser))
+}
+
 func main() {
-	rl := NewRateLimiter(10, 5, 5*time.Minute) // 10 burst, 5 req/sec
+	ctx, cancel := context.WithCancel(context.Background())
+	rl := NewRateLimiyter(10, 5, 30*time.Second, ctx)
+	defer cancel()
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+		w.Write([]byte("hello world"))
 	})
 
 	handler := rl.Middleware(mux)
 
-	fmt.Println("Server running on :8080")
+	fmt.Println("server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
