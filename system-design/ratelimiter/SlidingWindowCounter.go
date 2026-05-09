a57ec81f734daf2f3cd8b7de80edf897635c69bf
Vikas Singh <56144808+vikaassingh@users.noreply.github.com>
2026-05-10 00:56:00 +0530

    Update SlidingWindowCounter.go

--- system-design/ratelimiter/SlidingWindowCounter.go
+++ system-design/ratelimiter/SlidingWindowCounter.go
@@ -6,45 +6,43 @@ import (
 	"time"
 )
 
-type SlidingWindowLimiter struct {
-	mu sync.Mutex
-
+type SlidingWindowCounter struct {
+	mu             sync.Mutex
 	limit          int
 	windowSize     time.Duration
+	windowStart    time.Time
 	currentWindow  int
 	previousWindow int
-	windowStart    time.Time
 }
 
-func NewSlidingWindowLimiter(limit int, windowSize time.Duration) *SlidingWindowLimiter {
-	return &SlidingWindowLimiter{
+func NewSlidingWindowCounter(limit int, windowSize time.Duration) *SlidingWindowCounter {
+	return &SlidingWindowCounter{
 		limit:       limit,
 		windowSize:  windowSize,
 		windowStart: time.Now(),
 	}
 }
 
-func (s *SlidingWindowLimiter) Allow() bool {
+func (s *SlidingWindowCounter) Allow() bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	now := time.Now()
 	elapsed := now.Sub(s.windowStart)
 
-	// Move to next window
 	if elapsed >= s.windowSize {
-		s.previousWindow = s.currentWindow
+		if elapsed >= 2*s.windowSize {
+			s.previousWindow = 0
+		} else {
+			s.previousWindow = s.currentWindow
+		}
 		s.currentWindow = 0
-		s.windowStart = now
+		s.windowStart = s.windowStart.Add(s.windowSize)
 		elapsed = 0
 	}
 
-	// Calculate overlap ratio
 	overlapRatio := float64(s.windowSize-elapsed) / float64(s.windowSize)
-
-	// Effective count
-	effectiveCount := float64(s.previousWindow)*overlapRatio +
-		float64(s.currentWindow)
+	effectiveCount := float64(s.currentWindow) + float64(s.previousWindow)*overlapRatio
 
 	if effectiveCount >= float64(s.limit) {
 		return false
@@ -55,11 +53,10 @@ func (s *SlidingWindowLimiter) Allow() bool {
 }
 
 func main() {
-	limiter := NewSlidingWindowLimiter(5, 10*time.Second)
-
+	rl := NewSlidingWindowCounter(5, 10*time.Second)
 	for i := 1; i <= 10; i++ {
-		if limiter.Allow() {
-			fmt.Printf("Request %d allowed\n", i)
+		if rl.Allow() {
+			fmt.Printf("Request %d allowed \n", i)
 		} else {
 			fmt.Printf("Request %d blocked\n", i)
 		}
